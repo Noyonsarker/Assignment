89ee9c326a7144e811241bab0bbc52bf76fce682
Noyon <sarkernoyon62@gmail.com>
2023-01-23 15:55:58 +0600

    assignment-complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,41 @@
-// !Start-preloader
+// !Preloader
 window.onload = function () {
-  var load = document.getElementById("preloader");
-  window.addEventListener("load");
+  const preloaderArea = document.getElementById("preloader-area");
+  preloaderArea.className = "page-loaded";
+  setTimeout(function () {
+    preloaderArea.style.display = "none";
+  }, 500);
 };
+// !!!
+// !Email Js
+const submitButton = document.getElementById("submitButton");
 
-// !End-preloader
+submitButton.addEventListener("click", function () {
+  const fullNameValue = document.getElementById("contactName");
+  const emailValue = document.getElementById("contactEmail");
+  const messageValue = document.getElementById("contactMessage");
+
+  const valueFullName = fullNameValue.value;
+  const valueEmail = emailValue.value;
+  const valueMessage = messageValue.value;
+
+  fullNameValue.value = "";
+  emailValue.value = "";
+  messageValue.value = "";
+  const templateParams = {
+    valueFullName: valueFullName,
+    valueEmail: valueEmail,
+    valueMessage: valueMessage,
+  };
+
+  if ((valueFullName, valueEmail, valueMessage)) {
+    console.log(valueFullName, valueEmail, valueMessage);
+    emailjs.send(
+      "service_xtwjvlb",
+      "template_yokdx9g",
+      templateParams,
+      "rA0oDCbC4JLEc_9IQ"
+    );
+  }
+});
+// !!!
